c02a7af0153ca537ce214c9f140436e238c2c380
Jovert Palonpon <jovertical@gmail.com>
2020-09-15 23:32:21 +0800

    chore(deps) update packages + removed app.json

--- app.config.js
+++ app.config.js
@@ -2,10 +2,23 @@ import 'dotenv/config'
 
 export default {
   name: 'Mastdt',
+  slug: 'mastdt',
+  platforms: ['android'],
   version: '1.0.0',
-  extra: {},
+  orientation: 'landscape',
+  icon: './src/assets/icon.png',
+  splash: {
+    image: './src/assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
   android: {
     package: 'com.mastdt.mastdt',
     versionCode: 2,
   },
+  extra: {},
 }
